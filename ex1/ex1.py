5d2619c8a6fe02a32f923c3011fb65aa76496dd2
Caicai Zhang <hongboing@Hongbo-Jings-MacBook-Pro.local>
2017-05-02 19:30:15 -0400

    git init and ex1

---
+++ ex1/ex1.py
@@ -0,0 +1,9 @@
+#This is a comment out line and it won't be executed.
+print "Hello World!"
+print "Hello Again"
+print "I like typing this"
+print "This is fun."
+print 'Yay! Printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
+print "Another line."
