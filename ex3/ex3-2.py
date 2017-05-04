29225b3d377f639603177a2c6ce0ea2f14145bb6
Caicai Zhang <hongboing@Hongbo-Jings-MacBook-Pro.local>
2017-05-04 10:58:30 -0400

    ex3

---
+++ ex3/ex3-2.py
@@ -0,0 +1,42 @@
+# Rewirtten from ex3.py file to use floating point number.
+# print some content.
+print "I will now count my chickens."
+
+# print some nouns and do some calculation, especially with slash and percent.
+# How does % work? X divided by Y with J remaining. 100 divided by 16 with 4 remaining.
+print "Hens", 25.0 + 30.0 / 6
+print "Roosters", 100.0 - 25.0 * 3 % 4
+
+# print something.
+print "Now I will count the eggs:"
+
+# calculate with add, minus, time and divider and even some left number.
+print 3 + 2 + 1 - 5 + 4.0 % 2 - 1.0 / 4 + 6
+
+# print something.
+print "Is is true that 3 + 2 < 5 - 7?"
+
+# compare to see true or false.
+print 3 + 2 < 5 - 7
+
+# print the result of two calculation.
+print "What is 3 + 2?", 3 + 2
+print "What is 5 - 7?", 5 - 7
+
+# print something.
+print "Oh, that's why it's False."
+
+# print something.
+print "How about some more."
+
+# compare to see true or false.
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is is less or equal?", 5 <= -2
+
+print "What is 1 / 4?", 1 / 4
+print "What is 1.0 / 4?", 1.0 / 4
+
+print "What's the result of 7 / 4 ?", 7 / 4 # 1
+print "What's the result of 7.0 / 4.0 ?", 7.0 / 4.0 # 1.75
+print "See? it's not rounded down or up, it's just dropping the fractional part after the decimal."
---
+++ ex3/ex3.py
@@ -0,0 +1,34 @@
+# print some content.
+print "I will now count my chickens."
+
+# print some nouns and do some calculation, especially with slash and percent.
+# How does % work? X divided by Y with J remaining. 100 divided by 16 with 4 remaining.
+print "Hens", 25 + 30 / 6
+print "Roosters", 100 - 25 * 3 % 4
+
+# print something.
+print "Now I will count the eggs:"
+
+# calculate with add, minus, time and divider and even some left number.
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+# print something.
+print "Is is true that 3 + 2 < 5 - 7?"
+
+# compare to see true or false.
+print 3 + 2 < 5 - 7
+
+# print the result of two calculation.
+print "What is 3 + 2?", 3 + 2
+print "What is 5 - 7?", 5 - 7
+
+# print something.
+print "Oh, that's why it's False."
+
+# print something.
+print "How about some more."
+
+# compare to see true or false.
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is is less or equal?", 5 <= -2
